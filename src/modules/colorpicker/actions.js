de8edcf9ba6f9fe2ef13e78dc98d1428ab90545a
przemyslaw.lach <lprzemek@gmail.com>
2019-03-09 17:32:31 +0100

    normalize colors, hex to rgba helper

--- src/modules/colorpicker/actions.js
+++ src/modules/colorpicker/actions.js
@@ -1,10 +1,12 @@
 import * as type from './actionTypes';
 
+const normalizeColors = color => ({ ...color, hex: `#${color.hex}` });
+
 const fetchColors = () => ({
   type: type.FETCH_COLORS,
-  payload: fetch(process.env.REACT_APP_DEFAULT_API_URL).then(response =>
-    response.json()
-  )
+  payload: fetch(process.env.REACT_APP_DEFAULT_API_URL)
+    .then(response => response.json())
+    .then(data => data.map(normalizeColors))
 });
 
 export { fetchColors as default, fetchColors };
